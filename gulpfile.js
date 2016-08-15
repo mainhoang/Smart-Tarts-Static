7609f199a06c785ddab4ec4374a1342994e840e6
elon <elon@drawingfrommemory.com>
2016-08-15 13:53:57 -0700

    initial

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+const gulp = require('gulp')
+const browserSync = require('browser-sync').create()
+const sass = require('gulp-sass')
+
+gulp.task('serve', ['sass', 'html'], function() {
+  browserSync.init({
+    server: './dist',
+    port:1337
+  })
+  gulp.watch('src/**', ['html'])
+  gulp.watch('src/sass/*.scss', ['sass'])
+  gulp.watch('dist/*.html').on('change', browserSync.reload)
+})
+gulp.task('html', function(){
+  gulp.src('src/html/*.html').pipe(gulp.dest('dist'))
+  gulp.src('src/js/*.js').pipe(gulp.dest('dist/js'))
+})
+gulp.task('sass', function() {
+  return gulp.src('src/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream())
+})
+
+gulp.task('default', ['serve'])
+
+// example task construction
+// gulp.task('{command line command}', ['{task that will run}'])
