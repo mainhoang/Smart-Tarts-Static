e67bf7061c4f180b8a092e40ad59ba5358b24a57
David Johnson <davidjohnson1297@gmail.com>
2016-08-15 23:08:38 -0700

    updated to include the use of liquid partials

---
+++ dumbdata.js
@@ -0,0 +1,5 @@
+module.exports = {
+  first_name: 'Mister',
+  last_name: 'Fancy Pantsy',
+  image_path: './img/OrlandoMusic.gif'
+}
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,29 @@
 const gulp = require('gulp')
 const browserSync = require('browser-sync').create()
 const sass = require('gulp-sass')
+const liquify = require('gulp-liquify')
 
-gulp.task('serve', ['sass', 'move'], function() {
+// NOTE: dumbdata will not reload when browserSync is triggered as it is a build level dependancy
+var dumbdata = require('./dumbdata')
+
+
+gulp.task('serve', ['sass', 'move', 'liquify'], function() {
   browserSync.init({
     server: './dist',
     port:1337
   })
+  gulp.watch('src/**', ['liquify'])
   gulp.watch('src/**', ['move'])
   gulp.watch('src/sass/*.scss', ['sass'])
-  gulp.watch('dist/*.html').on('change', browserSync.reload)
+  gulp.watch('dist/**').on('change', browserSync.reload)
+})
+gulp.task('liquify', () => {
+  console.log('liquid template compilation example')
+  gulp.src('./src/html/*.html')
+      .pipe(liquify(dumbdata, { base: "./src/html/liquid-partials/" }))
+      .pipe(gulp.dest('./dist/'))
 })
 gulp.task('move', function(){
-  gulp.src('src/html/*.html').pipe(gulp.dest('dist'))
   gulp.src('src/js/*.js').pipe(gulp.dest('dist/js'))
   gulp.src('src/sass/fonts/**').pipe(gulp.dest('dist/css/fonts'))
   gulp.src('src/img/**').pipe(gulp.dest('dist/img'))
