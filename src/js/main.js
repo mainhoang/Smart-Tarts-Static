85ba1d752237e01c2f1a93af49f61365e305786e
mainhoang <mnhoang.80@gmail>
2016-08-27 00:02:35 -0700

    styling mobile. added hamburger

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,19 @@
 
 (function(){
-  'use strict';
-  // best place to keep finished script (closure)
-})();
 
+  document.addEventListener('DOMContentLoaded', function () {
+    document.querySelector('body').style.opacity = '1'
+  'use strict';
+
+})
+  // best place to keep finished script (closure)
+  
+  var burger = document.querySelector('.hamburger-menu')
+  var bar = document.querySelector('.bar')
+
+  burger.addEventListener('click', function () {
+    bar.classList.toggle('animate')
+  })
+
+})();
 console.log('js works')
