30c843e0db8a65e516af1c7fb01eca9e3dc327a0
mainhoang <mnhoang.80@gmail>
2016-08-31 16:11:30 -0700

    styled product-details & added dropin menu

--- src/js/main.js
+++ src/js/main.js
@@ -7,12 +7,16 @@
 
 })
   // best place to keep finished script (closure)
-  
+
   var burger = document.querySelector('.hamburger-menu')
   var bar = document.querySelector('.bar')
+  var logo = document.querySelector('.logo')
+  var dropin = document.querySelector('.dropin')
 
   burger.addEventListener('click', function () {
     bar.classList.toggle('animate')
+    dropin.classList.toggle('animate-menu')
+    logo.classList.toggle('hide-menu-item')
   })
 
 })();
