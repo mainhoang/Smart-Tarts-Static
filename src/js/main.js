7a77b35794998b336040ffdc7aa35701348b7837
mainhoang <mnhoang.80@gmail>
2016-09-02 16:39:07 -0700

    debug menu interaction

--- src/js/main.js
+++ src/js/main.js
@@ -14,6 +14,7 @@
   var centerLinks = document.querySelector(".center-links")
   var dropin = document.querySelector('.drop-in')
   var cart = document.querySelector(".cart")
+  var burgerCart = document.querySelector(".burger-cart")
   var basket = document.querySelector(".basket")
   var basketX = document.querySelector(".close-basket")
   var notMasked = document.querySelector(".no-mask")
@@ -22,17 +23,24 @@
   burger.addEventListener('click', function () {
     bar.classList.toggle('animate')
     dropin.classList.toggle('animate-menu')
+    
+  })
+
+  burgerCart.addEventListener("click", function(){
+    basket.classList.add("slide");
+    notMasked.classList.add("mask");
   })
 
   cart.addEventListener('click', function () {
-    basket.style.right = "0";
+    basket.classList.add("slide");
     logo.style.opacity = "0";
     centerLinks.style.opacity = "0";
     notMasked.classList.add("mask");
   })
 
   basketX.addEventListener('click', function () {
-    basket.style.right = "-100%";
+    basket.classList.remove("slide");
+    basket.classList.add("close-basket");
     logo.style.opacity = "1";
     centerLinks.style.opacity = "1";
     notMasked.classList.remove("mask");
